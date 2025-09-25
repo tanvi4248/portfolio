07d71489dfe910e30807a7e8cc5472ab5c327c50
tanvi4248 <tanvi@GWTN141-10.localdomain>
2025-09-25 13:42:56 -0500

    update download to link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import design1 from "../assets/design1.png";
 import design2 from "../assets/design2.png";
 import design3 from "../assets/design3.jpeg";
 import design4 from "../assets/design4.jpeg";
+import design5 from "../assets/design5.png";
 export default function Projects() {
   const [expandedProjects, setExpandedProjects] = useState([
     false,
@@ -20,7 +21,7 @@ export default function Projects() {
       return newState;
     });
   };
-
+  const description6 = `Treevah is a next-generation file management system designed to offer a faster, smarter, and more user-friendly alternative to traditional file explorers. By combining modern UI/UX with intelligent backend logic, Treevah helps users organize, locate, and interact with their files in a seamless way—no more endless clicking or hunting through subfolders.`;
   const description1 = `The Midwest Pumpkins project is a fully responsive web application dedicated to celebrating the beauty and versatility of pumpkins during the fall season.`;
   const description2 = `BLeaf makes self-care easy by giving users the support they need to meet their personal care goals. This app allows users to keep track of their personal care activities throughout the day and gives them the option to set up reminders, guilt and pressure free. While finding time to set goals and sticking to healthy habits can be difficult, it is extremely helpful to a person’s well-being. Self-care can help manage stress, lower the risk of illness, increase energy, and elevate moods. This is why bLeaf rewards positive habits day-by-day rather than promoting shame in a user for not always meeting their goals. A flower pot is provided to each user upon creating an account. In exchange for completing healthy habits, the user will see their flower grow. BLeaf celebrates even the small victories, aiming to help users bLeaf in themselves. Compatible with IOS mobile devices.`;
   const description3 = `This project is an Online Library Management System built using React.js, Redux, and Tailwind CSS. It allows users to browse, add, and manage books in an online library. The application includes a responsive UI and uses React Router for navigation.`;
@@ -39,6 +40,52 @@ export default function Projects() {
       </h3>
       <div className="projects-inner grid lg:grid-cols-3 gap-4 md:grid-cols-2">
         {/* Project 1 */}
+        <div className="project rounded-lg p-5 bg-zinc-800 max-[480px]:p-2">
+          <div className="img pb-5 max-[480px]:pb-3">
+            <a href="">
+              <img src={design5} className="rounded-lg" />
+            </a>
+          </div>
+          <div className="desc text-left">
+            <strong className="pb-2 text-xl max-[480px]:text-lg">
+              Treevah - The File Management
+            </strong>
+            <div className="pb-2">Role: Full Stack Developer</div>
+            <div className="description pb-2">
+              <div>
+                Description:{" "}
+                {expandedProjects[0]
+                  ? description6
+                  : `${description6.slice(0, maxLength)}...`}
+              </div>
+              <button
+                onClick={() => toggleExpand(0)}
+                className="text-orange-600 see-more-button"
+              >
+                {expandedProjects[0] ? "See Less" : "See More"}
+              </button>
+            </div>
+            <div className="pb-2">
+              Technologies: React.js, Redux Toolkit, Node.js, Express.js,
+              PostgreSQL, Redis, Azure, REST APIs, HTML5, CSS3, JavaScript
+            </div>
+            <div className="buttons-group flex pb-2">
+              {/* <a
+                href="https://treevah.vercel.app/login"
+                className="flex mr-2 justify-center rounded-full bg-orange-600 px-4 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Website
+              </a> */}
+              <a
+                className="flex justify-center rounded-full bg-orange-600 px-4 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                href="https://github.com/filter-folder/filterfolder-cloud"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+        </div>
+
         <div className="project rounded-lg p-5 bg-zinc-800 max-[480px]:p-2">
           <div className="img pb-5 max-[480px]:pb-3">
             <a href="">
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,16 +1,14 @@
 export default function Resume() {
-  const handleDownload = () => {
-    const link = document.createElement("a");
-    link.href = `/Tanvi Patel_Resume.pdf`; // Path to the file in the public folder
-    link.download = "Tanvi Patel_Resume.pdf"; // Suggested filename
-    link.click();
+  const handleRedirect = () => {
+    window.open("/Tanvi Patel_Resume.pdf", "_blank");
   };
+
   return (
     <button
       className="download-btn rounded-full bg-orange-600 px-3 py-1.5"
-      onClick={handleDownload}
+      onClick={handleRedirect}
     >
-      Download Resume
+      View Resume
     </button>
   );
 }
